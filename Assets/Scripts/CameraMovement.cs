63799178ebbb8c061b1bc1a69d96b5ff2b996b73
LordRivero <carlosrive99@gmail.com>
2024-02-12 13:54:44 +0100

    Cambios Cámara 12 de febrero.
    La cámara ahora sigue al jugador en el eje de las X y de las Y en el script de CameraMovement.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject target; //Referncia al target, el personaje.
+    public float smoothSpeed = 0.125f; //Velocidad de suavizado.
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+        Vector3 desiredPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); //Calcula la posición deseada de la cámara.
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); //Interpola suavemente la posicion actual de la camara hacia la posición deseada.
+
+        transform.position = desiredPosition; //Actualiza la posicion de la cámara.
     }
 }
+    //void Update()
+    //{
+        //transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+    //}
+//}
